2d374c244165f948987e00118a5f82b213a62933
shazaib <dev424647@gmail.com>
2022-06-09 18:19:14 +0500

    updates

--- main.js
+++ main.js
@@ -47,3 +47,25 @@ let typed2 = new Typed(".typing-2", {
     backSpeed: 60,
     loop: true
 });
+
+$(".owl-carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0 : {
+            items: 1,
+            nav: false
+        },
+        600: {
+            items: 2,
+            nav: false
+        },
+        1000: {
+            items: 3,
+            nav: false
+        }
+    }
+})
